de5d6f08fb4bad876477da646b83a8f1bbe1cbac
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-11 14:42:45 +0100

    Added deta init

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 import os
+from deta import Deta
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
 load_dotenv()
 app = FastAPI()
 isDev = os.getenv("DEBUG") is not None
+TABLE_NAME = "cocktails" + ("_dev" if isDev else "")
+deta = Deta(os.getenv("DETA_PROJECT_KEY"))
+cocktails = deta.Base(TABLE_NAME)
 
 
 @app.get("/")
