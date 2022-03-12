d1b44fd1e35beafee051eab15e36fcbdc94f0d11
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-12 14:15:04 +0100

    Added cocktail post route

--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,26 @@ from deta import Deta
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
+from models import CocktailData
+
 load_dotenv()
 app = FastAPI()
 isDev = os.getenv("DEBUG") is not None
 TABLE_NAME = "cocktails" + ("_dev" if isDev else "")
 deta = Deta(os.getenv("DETA_PROJECT_KEY"))
-cocktails = deta.Base(TABLE_NAME)
+cocktail_deta = deta.Base(TABLE_NAME)
 
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the API of the CocktailBerry-WebApp"}
+
+
+@app.post("/cocktail")
+def insert_cocktaildata(cocktail: CocktailData):
+    return cocktail_deta.insert({
+        "cocktailname": cocktail.cocktailname,
+        "volume": cocktail.volume,
+        "machinename": cocktail.machinename,
+        "countrycode": cocktail.countrycode
+    })
