a40815aaff5693b7215cbc150d0135be2ec4e373
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-13 13:37:20 +0100

    Added styles

--- frontend/styles.py
+++ frontend/styles.py
@@ -1,7 +1,18 @@
+import streamlit as st
+
 # CSS to inject contained in a string
-hide_table_row_index = """
+STYLESETTINGS = """
             <style>
             tbody th {display:none}
             .blank {display:none}
+            .appview-container .main .block-container{
+                padding-top: 1rem;
+                max-width: 1000px;
+            }
             </style>
             """
+
+
+def generate_style():
+    """Style for Centered mode that app gets more width"""
+    st.markdown(STYLESETTINGS, unsafe_allow_html=True)
