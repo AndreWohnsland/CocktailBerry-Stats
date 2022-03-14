6737f5ae829410f66da33234bc5222d5157c95a8
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-14 19:29:36 +0100

    Added warnings if filter return no data

--- frontend/views.py
+++ frontend/views.py
@@ -17,14 +17,20 @@ class DataFrameStats():
 
 def generate_sidebar(df: pd.DataFrame):
     """Generates the sidebar with the option. Returns needed variables"""
-    st.sidebar.header("Filter CocktailBerry Data")
+    st.sidebar.header("🔍 Filter CocktailBerry Data")
     st.sidebar.write("Here you can limit the data and filter a little bit.")
+    if df.empty:
+        st.sidebar.write("Nothing to do, need some data ...")
+        return [], [], [], 1, DataFrameStats(0, 0, 0, 0, 0)
     st.sidebar.subheader("Filter Options")
     country_selection = sorted(list(df["Language"].unique()))
-    countrycodes = st.sidebar.multiselect("Choose Countrycodes:", country_selection, country_selection)
+    countrycodes = st.sidebar.multiselect("Choose Used Languages:", country_selection, country_selection)
     machine_selection = sorted(list(df["Machine Name"].unique()))
     machines = st.sidebar.multiselect("Choose Machines:", machine_selection, machine_selection)
     recipes_selection = sorted(list(df["Cocktailname"].unique()))
+    recipes_limit = st.sidebar.slider(
+        "Show x most popular recipes:", 2, max(2, len(recipes_selection)), min(10, len(recipes_selection))
+    )
     recipes = st.sidebar.multiselect("Choose Recipes:", recipes_selection, recipes_selection)
     # also generates the needed data out of the df
     # since we got unique calculation already here (to save some compute things)
@@ -35,10 +41,11 @@ def generate_sidebar(df: pd.DataFrame):
         len(df),
         df["Volume"].sum() / 1000,
     )
-    return countrycodes, machines, recipes, df_stats
+    return countrycodes, machines, recipes, recipes_limit, df_stats
 
 
 def display_introduction(df_stats: DataFrameStats):
+    """Displays some basic information and stats about the data & project"""
     st.title("🍹CocktailBerry Dashboard")
     st.markdown("Dashboard for all the [CocktailBerry](https://github.com/AndreWohnsland/CocktailBerry) machines data!")
     with st.expander("What is this?"):
@@ -47,15 +54,16 @@ def display_introduction(df_stats: DataFrameStats):
         f"""
         # 📈 Current CocktailBerry Stats
         - 🍸 **{df_stats.cocktails}** cocktails made
+        - 🧾 **{df_stats.recipes}** different recipes tasted
         - 🎊 **{df_stats.volume:.1f}** litre cocktails produced
         - 🕹️ **{df_stats.machines}** machines sending data
         - 🌐 **{df_stats.countries}** languages used
-        - 🧾 **{df_stats.recipes}** different recipes produced
         """
     )
 
 
 def __what_is_this():
+    """Returns some additional information about the dashboard"""
     st.markdown(
         """
         This is the official WebApp / Dashboard for the [CocktailBerry](https://github.com/AndreWohnsland/CocktailBerry) project.
@@ -71,26 +79,49 @@ def __what_is_this():
     )
 
 
-def display_data(df: pd.DataFrame, filterd_df: pd.DataFrame):
+def display_data(df: pd.DataFrame, filterd_df: pd.DataFrame, recipes_limit: int):
+    """Generates all the data views (plots and tables) from the data"""
     volume_df = sum_volume(filterd_df)
-    recipe_df = cocktail_count(filterd_df)
+    recipe_df = cocktail_count(filterd_df, recipes_limit)
+
+    # Display section of volume / count data
     st.header("🍸 Volume and Number of Cocktails")
-    generate_volume_treemap(volume_df)
+    if not volume_df.empty:
+        generate_volume_treemap(volume_df)
+    else:
+        __say_no_data()
     with st.expander("[Table] Aggregated by Language used and Machine Name:"):
         st.table(volume_df.style.format({"Cocktail Volume in Litre": "{:.2f}"}))
 
+    # Display section of recipe data
     st.header("🧾 Recipes Made")
-    generate_recipes_treemap(recipe_df)
-    with st.expander("[Table] Aggregated by Recipe Name and Language used:"):
+    if not recipe_df.empty:
+        generate_recipes_treemap(recipe_df)
+    else:
+        __say_no_data()
+    with st.expander(f"[Table] Aggregated by Recipe Name and Language used (Top {recipes_limit}):"):
         st.table(recipe_df)
 
+    # show dev thingies if devmode is on
     if is_dev:
         st.header("⚙️ Debug Stuff")
         with st.expander("All raw Data:"):
             st.table(df)
 
 
+def __say_no_data():
+    """Displays a warning that there is no data to plot"""
+    st.warning(
+        """
+        ⚠️ There is currently no data to be displayed. This is probably a result of following reason:
+
+        - ❌ Your filtering is too strict and nothing matches the criteria. Change your filter or reload the page to reset the filtering.
+        """
+    )
+
+
 def display_footer():
+    """Generates the footer element with from HTML data"""
     footer = """
     <div class="footer">
         <p class="left">Developed with ❤️</p>
