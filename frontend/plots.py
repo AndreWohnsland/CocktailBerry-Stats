2475a17546fc43bb9781b288f87548f451427851
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-14 17:03:14 +0100

    Styling for plots

--- frontend/plots.py
+++ frontend/plots.py
@@ -8,14 +8,22 @@ warnings.filterwarnings("ignore")
 
 
 def generate_volume_treemap(df: pd.DataFrame):
-    fig = px.treemap(df, path=[px.Constant("Countrycodes"), 'Countrycode', 'Machinename'],
-                     values="Cocktail Volume in ml", height=400)
+    fig = px.treemap(df, path=[px.Constant("Language used"), 'Language', 'Machine Name'],
+                     values="Cocktail Volume in Litre", height=400)
     fig.update_layout({"margin": {"l": 0, "r": 0, "t": 0, "b": 0}})
-    st.plotly_chart(fig, use_container_width=True)
+    fig.update_traces(
+        texttemplate="<b>%{label}</b><br>%{value:,.2f} l",
+        hovertemplate='%{label}<br>Cocktail Volume: %{value:,.2f} l'
+    )
+    st.plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
 
 
 def generate_recipes_treemap(df: pd.DataFrame):
-    fig = px.treemap(df, path=[px.Constant("Recipes"), "Cocktailname", 'Countrycode'],
+    fig = px.treemap(df, path=[px.Constant("Recipes"), "Cocktailname", 'Language'],
                      values="Number of Cocktails", height=400)
     fig.update_layout({"margin": {"l": 0, "r": 0, "t": 0, "b": 0}})
-    st.plotly_chart(fig, use_container_width=True)
+    fig.update_traces(
+        texttemplate="<b>%{parent}</b> <i>x</i>%{value:.0f}<br>(%{label})",
+        hovertemplate='%{parent} (%{label})<br>Repice made: %{value:,.0f}'
+    )
+    st.plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
