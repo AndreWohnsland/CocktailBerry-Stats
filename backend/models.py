f6472719fbd834250c2e2322c027068e96becb67
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-14 19:23:42 +0100

    Added enums for countrycodes

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,14 @@
+from enum import Enum
 from pydantic import BaseModel
+
+
+class LandEnum(str, Enum):
+    en = 'en'
+    de = 'de'
 
 
 class CocktailData(BaseModel):
     cocktailname: str
     volume: int
     machinename: str
-    countrycode: str
+    countrycode: LandEnum
