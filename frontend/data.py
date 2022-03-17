4c20417d84a8f8d0e7b4c19485f8a247edccc5ec
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-17 16:49:38 +0100

    Using dataclass to manage df names

--- frontend/data.py
+++ frontend/data.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass
 import streamlit as st
 from deta import Deta
 import pandas as pd
@@ -11,10 +12,18 @@ TABLE_NAME = "cocktails" + ("_dev" if is_dev else "")
 deta = Deta(os.getenv("DETA_BASE_KEY"))
 cocktail_deta = deta.Base(TABLE_NAME)
 
-MACHINENAME = "Machine Name"
-COCKTAILNAME = "Cocktailname"
-COCKTAILCOUNT = "Number of Cocktails"
-COCKTAILVOLUME = "Cocktail Volume in Litre"
+
+@dataclass
+class DfNames():
+    machine_name: str = "Machine Name"
+    cocktail_name: str = "Cocktail Name"
+    cocktail_count: str = "Number of Cocktails"
+    cocktail_volume: str = "Cocktail Volume in Litre"
+    volume: str = "Volume"
+    language: str = "Language"
+
+
+dfnames = DfNames()
 
 
 @st.cache(ttl=60)
@@ -22,13 +31,18 @@ def generate_df():
     """Gets the data from deta and converts to df"""
     cocktails = cocktail_deta.fetch().items
     df = pd.DataFrame(cocktails).rename(columns={
-        "countrycode": "Language",
-        "machinename": MACHINENAME,
-        "cocktailname": "Cocktailname",
-        "volume": "Volume",
+        "countrycode": dfnames.language,
+        "machinename": dfnames.machine_name,
+        "cocktailname": dfnames.cocktail_name,
+        "volume": dfnames.volume,
     })
     if not df.empty:
-        df = df[["Language", MACHINENAME, "Cocktailname", "Volume"]]
+        df = df[[
+            dfnames.language,
+            dfnames.machine_name,
+            dfnames.cocktail_name,
+            dfnames.volume,
+        ]]
     return df
 
 
@@ -36,9 +50,9 @@ def generate_df():
 def filter_dataframe(df: pd.DataFrame, countries: list, machines: list, recipes: list):
     """Applies the sidebar filter option to the data"""
     filtered_df = df.loc[
-        df["Language"].isin(countries) &
-        df[MACHINENAME].isin(machines) &
-        df["Cocktailname"].isin(recipes)
+        df[dfnames.language].isin(countries) &
+        df[dfnames.machine_name].isin(machines) &
+        df[dfnames.cocktail_name].isin(recipes)
     ]
     return filtered_df
 
@@ -46,29 +60,30 @@ def filter_dataframe(df: pd.DataFrame, countries: list, machines: list, recipes:
 @st.cache(ttl=60)
 def sum_volume(df: pd.DataFrame) -> pd.DataFrame:
     """Aggregate by language and machine Name, returns total volumes and cocktail counts"""
-    volumes = df.groupby(["Language", MACHINENAME])["Volume"] \
+    volumes = df.groupby([dfnames.language, dfnames.machine_name])[dfnames.volume] \
         .agg(["sum", "count"]).reset_index() \
         .sort_values(['sum', 'count'], ascending=False) \
         .rename(columns={
-            "sum": COCKTAILVOLUME,
-            "count": COCKTAILCOUNT,
+            "sum": dfnames.cocktail_volume,
+            "count": dfnames.cocktail_count,
         })
-    volumes[COCKTAILVOLUME] = volumes[COCKTAILVOLUME] / 1000
+    volumes[dfnames.cocktail_volume] = volumes[dfnames.cocktail_volume] / 1000
     return volumes
 
 
 @st.cache(ttl=60)
 def cocktail_count(df: pd.DataFrame, limit_recipe: int = 10) -> pd.DataFrame:
     """Aggregate by language and cocktailname, limits to x most used recipes"""
-    nameorder = df.groupby(["Cocktailname"])["Volume"].count().sort_values().index.to_list()[-limit_recipe:]
+    nameorder = df.groupby([dfnames.cocktail_name])[dfnames.volume].count(
+    ).sort_values().index.to_list()[-limit_recipe:]
     sorter_index = dict(zip(nameorder, range(len(nameorder))))
-    cocktails = df.groupby(["Cocktailname", "Language"])["Volume"] \
+    cocktails = df.groupby([dfnames.cocktail_name, dfnames.language])[dfnames.volume] \
         .count().reset_index() \
         .rename(columns={
-            "Volume": COCKTAILCOUNT,
+            dfnames.volume: dfnames.cocktail_count,
         })
-    cocktails['Rank'] = cocktails['Cocktailname'].map(sorter_index)
-    cocktails.sort_values(['Rank', COCKTAILCOUNT], ascending=False, inplace=True)
+    cocktails['Rank'] = cocktails[dfnames.cocktail_name].map(sorter_index)
+    cocktails.sort_values(['Rank', dfnames.cocktail_count], ascending=False, inplace=True)
     cocktails.dropna(axis=0, inplace=True)
     cocktails.drop('Rank', 1, inplace=True)
     return cocktails
