59585a570d4a8accff42b65a38b0be00f38f50d0
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-18 19:06:51 +0100

    Added ui option for time filtering

--- frontend/views.py
+++ frontend/views.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 
-from data import sum_volume, cocktail_count, is_dev, dfnames
-from plots import generate_volume_treemap, generate_recipes_treemap
+import plots
+from data import sum_volume, cocktail_count, time_aggregation, is_dev, dfnames
 
 
 @dataclass
@@ -24,8 +24,9 @@ def generate_sidebar(df: pd.DataFrame):
     st.sidebar.write("Here you can limit the data and filter a little bit.")
     if df.empty:
         st.sidebar.write("Nothing to do, need some data ...")
-        return [], [], [], 1, DataFrameStats(0, 0, 0, 0, 0, "No Data", "No Data")
+        return [], [], [], 1, False, DataFrameStats(0, 0, 0, 0, 0, "No Data", "No Data")
     st.sidebar.subheader("Filter Options")
+    only_one_day = st.sidebar.checkbox("Only show last 24h data")
     country_selection = sorted(list(df[dfnames.language].unique()))
     countrycodes = st.sidebar.multiselect("Choose Used Languages:", country_selection, country_selection)
     machine_selection = sorted(list(df[dfnames.machine_name].unique()))
@@ -46,7 +47,7 @@ def generate_sidebar(df: pd.DataFrame):
         __build_date(df[dfnames.receivedate].min()),
         __build_date(df[dfnames.receivedate].max()),
     )
-    return countrycodes, machines, recipes, recipes_limit, df_stats
+    return countrycodes, machines, recipes, recipes_limit, only_one_day, df_stats
 
 
 def __build_date(checkdate: datetime) -> str:
@@ -93,7 +94,7 @@ def __what_is_this():
     )
 
 
-def display_data(filterd_df: pd.DataFrame, recipes_limit: int):
+def display_data(filterd_df: pd.DataFrame, recipes_limit: int, last_day: bool):
     """Generates all the data views (plots and tables) from the data"""
     volume_df = sum_volume(filterd_df)
     recipe_df = cocktail_count(filterd_df, recipes_limit)
@@ -101,7 +102,7 @@ def display_data(filterd_df: pd.DataFrame, recipes_limit: int):
     # Display section of volume / count data
     st.header("🍸 Volume and Number of Cocktails")
     if not volume_df.empty:
-        generate_volume_treemap(volume_df)
+        plots.generate_volume_treemap(volume_df)
     else:
         __say_no_data()
     with st.expander(f"[Table] Aggregated by Language used and {dfnames.machine_name}:"):
@@ -110,11 +111,34 @@ def display_data(filterd_df: pd.DataFrame, recipes_limit: int):
     # Display section of recipe data
     st.header("🧾 Recipes Made")
     if not recipe_df.empty:
-        generate_recipes_treemap(recipe_df)
+        plots.generate_recipes_treemap(recipe_df)
     else:
         __say_no_data()
     with st.expander(f"[Table] Aggregated by {dfnames.cocktail_name} and Language used (Top {recipes_limit}):"):
         st.table(recipe_df)
+
+    # Display time stats
+    st.header("⏱️ Data Over Time")
+    hour_grouping = __define_granularity(last_day)
+    if not filterd_df.empty:
+        time_df = time_aggregation(filterd_df, last_day, hour_grouping)
+        plots.generate_time_plot(time_df)
+    else:
+        __say_no_data()
+
+
+def __define_granularity(last_day):
+    """Lets the user choose in case of all data to aggregate by hour or day"""
+    if not last_day:
+        grouping_options = ("One Day", "One Hour")
+        selected_grouping = st.radio(
+            "Select the time grouping",
+            grouping_options
+        )
+        hour_grouping = selected_grouping == grouping_options[1]
+    else:
+        hour_grouping = False
+    return hour_grouping
 
 
 def __say_no_data():
@@ -157,42 +181,46 @@ def display_api_instructions():
         blog or social media post). Afterwards, you will get an API key to submit your machine data.
         """)
     with st.expander("Show details and reasons behind them:"):
-        st.markdown(
-            """
-            #### How To
+        __detailed_explanation()
 
-            The current procedure require you to build your own **CocktailBerry** machine and deliver some kind of proof of its existence.
-            The easiest way to do this is to submit a video or some photos of your machine.
-            The machine does **NOT** need to be fancy in any way, it should simply work, be able to make cocktails
-            and run the CocktailBerry software.
-            It does not matter if the pumps are showing or the cabling looks like a mess. 😉
 
-            Please upload your video / photos to some sort of hosting site (Imgur, etc.) and provide the link,
-            or give a reference to your blogpost (own blog, Reddit, social media) if you did such things for your machine.
-            It would be nice to write some words in addition to the submission, even if it's just a kind greeting,
-            and provide me some sort of name or alias how should I reply to you (first name or your preferred alias is fine).
-            You can [contact me](mailto:cocktailmakeraw@gmail.com) for further questions or just to get the API-key.
+def __detailed_explanation():
+    st.markdown(
+        """
+        #### How To
 
-            #### Why a protected API
+        The current procedure require you to build your own **CocktailBerry** machine and deliver some kind of proof of its existence.
+        The easiest way to do this is to submit a video or some photos of your machine.
+        The machine does **NOT** need to be fancy in any way, it should simply work, be able to make cocktails
+        and run the CocktailBerry software.
+        It does not matter if the pumps are showing or the cabling looks like a mess. 😉
 
-            The reason behind this is that only **real machines** should submit data for the dashboard (you could otherwise
-            just run the Python program anywhere and submit a lot of data) and to minimize any other kinds of exploits
-            of the API. There is no option to submit a photo (the implementation here would be quite simple) because this could
-            also be exploited quite easily. The internet is still a wild and scary place! 🦖
-            Also, I would love to see what you did in combination with my software.
+        Please upload your video / photos to some sort of hosting site (Imgur, etc.) and provide the link,
+        or give a reference to your blogpost (own blog, Reddit, social media) if you did such things for your machine.
+        It would be nice to write some words in addition to the submission, even if it's just a kind greeting,
+        and provide me some sort of name or alias how should I reply to you (first name or your preferred alias is fine).
+        You can [contact me](mailto:cocktailmakeraw@gmail.com) for further questions or just to get the API-key.
 
-            #### Final steps
+        #### Why a protected API
 
-            You can then use the received API key for the CocktailBerry microservice (in the .env file).
-            As soon as there is a valid key and the microservice is enabled, your machine will send the cocktail data
-            (*cocktail name, machine name, volume and language setting*) to the API using the API-key.
-            The activation of this feature is also explained in the CocktailBerry docs.
+        The reason behind this is that only **real machines** should submit data for the dashboard (you could otherwise
+        just run the Python program anywhere and submit a lot of data) and to minimize any other kinds of exploits
+        of the API. There is no option to submit a photo (the implementation here would be quite simple) because this could
+        also be exploited quite easily. The internet is still a wild and scary place! 🦖
+        Also, I would love to see what you did in combination with my software.
 
-            #### Will this be easier in the future
+        #### Final steps
 
-            I always work on improvements and try to bring the most joy with CocktailBerry and related projects.
-            Since I do it all in my free time + open source, sadly my time is limited, and I can only do so much.
-            If you may know a better way, feel free to contact me and let's start a nice discussion to make the
-            CocktailBerry environment even better!
-            """
-        )
+        You can then use the received API key for the CocktailBerry microservice (in the .env file).
+        As soon as there is a valid key and the microservice is enabled, your machine will send the cocktail data
+        (*cocktail name, machine name, volume and language setting*) to the API using the API-key.
+        The activation of this feature is also explained in the CocktailBerry docs.
+
+        #### Will this be easier in the future
+
+        I always work on improvements and try to bring the most joy with CocktailBerry and related projects.
+        Since I do it all in my free time + open source, sadly my time is limited, and I can only do so much.
+        If you may know a better way, feel free to contact me and let's start a nice discussion to make the
+        CocktailBerry environment even better!
+        """
+    )
