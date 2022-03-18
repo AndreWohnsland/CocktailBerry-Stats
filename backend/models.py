39b9a2226b8492869a2a526cb471f99c8d9d224c
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-18 19:05:09 +0100

    Some docs for models

--- backend/models.py
+++ backend/models.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 
 
 class LandEnum(str, Enum):
+    """Limits countrycodes to currently supported ones"""
     en = 'en'
     de = 'de'
 
 
 class CocktailData(BaseModel):
+    """Model for all needed cocktail data"""
     cocktailname: str
     volume: int
     machinename: str
