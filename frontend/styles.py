a43613d257d7b2c68b4c2a25c6ea7e97a0dc7fbc
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-02-13 18:46:33 +0100

    Fix spelling

--- frontend/styles.py
+++ frontend/styles.py
@@ -5,9 +5,9 @@ DIRPATH = Path(__file__).parent.absolute()
 
 # CSS to inject contained in a string
 with open(DIRPATH / 'style.css', encoding="utf-8") as f:
-    STYLESETTINGS = f"<style>{f.read()}</style>"
+    STYLE_SETTINGS = f"<style>{f.read()}</style>"
 
 
 def generate_style():
     """Style for Centered mode that app gets more width"""
-    st.markdown(STYLESETTINGS, unsafe_allow_html=True)
+    st.markdown(STYLE_SETTINGS, unsafe_allow_html=True)
