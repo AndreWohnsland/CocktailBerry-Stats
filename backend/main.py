f7ceae0518f7db5f676fad1cf7109d76de085099
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-06-18 17:00:00 +0200

    Fix linting in backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,10 @@
 import datetime
 from typing import Optional
 
+from app import init_app
 from fastapi import Header
 from fastapi.logger import logger
-from models import CocktailData, DetaCocktail, DetaEvent, InstallationData, DetaInstallation
-from app import init_app
-
+from models import CocktailData, DetaCocktail, DetaEvent, DetaInstallation, InstallationData
 
 app, deta, isDev = init_app()
 TABLE_NAME = "cocktails" + ("_dev" if isDev else "")
@@ -17,13 +16,14 @@ DATEFORMAT_STR = "%d/%m/%Y, %H:%M"
 
 @app.get("/", tags=["protected"])
 def check_api():
-    """Route to check if api is working"""
+    """Route to check if api is working."""
     return {"message": "Welcome to the API of the CocktailBerry-WebApp"}
 
 
 @app.post("/cocktail", tags=["cocktail", "protected"])
 def insert_cocktaildata(cocktail: CocktailData, x_deta_api_key_name: Optional[str] = Header(None)):
     """Insert the cocktail data into the database.
+
     Route is protected by API key.
     """
     return cocktail_deta.insert(
@@ -42,7 +42,9 @@ def insert_cocktaildata(cocktail: CocktailData, x_deta_api_key_name: Optional[st
 @app.get("/public/cocktails", tags=["cocktail", "open"], response_model=list[DetaCocktail])
 def get_cocktaildata() -> list[DetaCocktail]:
     """Get the cocktail data from the database.
-    Route is open accessible."""
+
+    Route is open accessible.
+    """
     res = cocktail_deta.fetch(limit=10000)
     cocktails: list[DetaCocktail] = res.items
     while res.last:
@@ -54,7 +56,9 @@ def get_cocktaildata() -> list[DetaCocktail]:
 @app.post("/public/installation", tags=["installation", "open"])
 def post_installation(information: InstallationData):
     """Endpoint to post information about successful installation.
-    Route is open accessible."""
+
+    Route is open accessible.
+    """
     return installation_deta.insert(
         {
             "receivedate": datetime.datetime.now().strftime(DATEFORMAT_STR),
@@ -66,7 +70,9 @@ def post_installation(information: InstallationData):
 @app.get("/public/installations", tags=["installation", "open"], response_model=list[DetaInstallation])
 def get_installations():
     """Endpoint to receive information about successful installation.
-    Route is open accessible."""
+
+    Route is open accessible.
+    """
     res = installation_deta.fetch(limit=10000)
     installations: list = res.items
     while res.last:
@@ -78,7 +84,9 @@ def get_installations():
 @app.get("/public/installations/count", tags=["installation", "open"])
 def get_installation_count():
     """Endpoint to receive information about successful installation count.
-    Route is open accessible."""
+
+    Route is open accessible.
+    """
     res = installation_deta.fetch(limit=10000)
     installations: list = res.items
     while res.last:
@@ -90,6 +98,7 @@ def get_installation_count():
 @app.post("/__space/v0/actions", tags=["automation", "protected"])
 def run_actions(event_object: DetaEvent) -> None:
     """Route which is triggered on deta action.
+
     The event data with id / trigger is provided in the event object.
     """
     event = event_object.event
