073ce12fd11a8699684b58c94f838cc4255ea234
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-09-02 21:40:59 +0200

    Switch from deta base to mongoDB

--- backend/main.py
+++ backend/main.py
@@ -1,111 +1,85 @@
 import datetime
-from typing import Optional
 
-from app import init_app
-from fastapi import Header
+from fastapi import APIRouter
 from fastapi.logger import logger
-from models import CocktailData, DetaCocktail, DetaEvent, DetaInstallation, InstallationData
+from models import CocktailDocument, InstallationDocument
+from schemas import CocktailData, InstallationData
 
-app, deta, isDev = init_app()
-TABLE_NAME = "cocktails" + ("_dev" if isDev else "")
-cocktail_deta = deta.Base(TABLE_NAME)
-_INSTALLATION_TBL = "installation" + ("_dev" if isDev else "")
-installation_deta = deta.Base(_INSTALLATION_TBL)
 DATEFORMAT_STR = "%d/%m/%Y, %H:%M"
 
+router = APIRouter(prefix="/api/v1", tags=["protected"])
+public_router = APIRouter(prefix="/api/v1/public", tags=["public"])
 
-@app.get("/", tags=["protected"])
-def check_api():
+
+@router.get("/", tags=["protected"])
+async def check_api():
     """Route to check if api is working."""
     return {"message": "Welcome to the API of the CocktailBerry-WebApp"}
 
 
-@app.post("/cocktail", tags=["cocktail", "protected"])
-def insert_cocktaildata(cocktail: CocktailData, x_deta_api_key_name: Optional[str] = Header(None)):
+@router.post("/cocktail", tags=["cocktail"])
+async def insert_cocktaildata(cocktail: CocktailData):
     """Insert the cocktail data into the database.
 
     Route is protected by API key.
     """
-    return cocktail_deta.insert(
-        {
-            "cocktailname": cocktail.cocktailname[:30],  # limit by 30 chars
-            "volume": cocktail.volume,
-            "machinename": cocktail.machinename[:30],  # limit by 30 chars
-            "countrycode": cocktail.countrycode,
-            "keyname": x_deta_api_key_name,
-            "makedate": cocktail.makedate,
-            "receivedate": datetime.datetime.now().strftime(DATEFORMAT_STR),
-        }
-    )
+    return await CocktailDocument(
+        cocktailname=cocktail.cocktailname[:30],  # limit by 30 chars
+        volume=cocktail.volume,
+        machinename=cocktail.machinename[:30],  # limit by 30 chars
+        countrycode=cocktail.countrycode,
+        keyname=None,
+        makedate=cocktail.makedate,
+        receivedate=datetime.datetime.now().strftime(DATEFORMAT_STR),
+    ).create()
 
 
-@app.get("/public/cocktails", tags=["cocktail", "open"], response_model=list[DetaCocktail])
-def get_cocktaildata() -> list[DetaCocktail]:
+@public_router.get("/cocktails", tags=["cocktail"], response_model=list[CocktailDocument])
+async def get_cocktaildata() -> list[CocktailDocument]:
     """Get the cocktail data from the database.
 
     Route is open accessible.
     """
-    res = cocktail_deta.fetch(limit=10000)
-    cocktails: list[DetaCocktail] = res.items
-    while res.last:
-        res = cocktail_deta.fetch(limit=10000, last=res.last)
-        cocktails += res.items
-    return cocktails
+    return await CocktailDocument.find_all().to_list()
 
 
-@app.post("/public/installation", tags=["installation", "open"])
-def post_installation(information: InstallationData):
+@public_router.post("/installation", tags=["installation"])
+async def post_installation(information: InstallationData):
     """Endpoint to post information about successful installation.
 
     Route is open accessible.
     """
-    return installation_deta.insert(
-        {
-            "receivedate": datetime.datetime.now().strftime(DATEFORMAT_STR),
-            "os": information.os_version,
-        }
-    )
+    return await InstallationDocument(
+        os=information.os_version,
+        receivedate=datetime.datetime.now().strftime(DATEFORMAT_STR)
+    ).create()
 
 
-@app.get("/public/installations", tags=["installation", "open"], response_model=list[DetaInstallation])
-def get_installations():
+@public_router.get("/installations", tags=["installation"], response_model=list[InstallationDocument])
+async def get_installations() -> list[InstallationDocument]:
     """Endpoint to receive information about successful installation.
 
     Route is open accessible.
     """
-    res = installation_deta.fetch(limit=10000)
-    installations: list = res.items
-    while res.last:
-        res = installation_deta.fetch(limit=10000, last=res.last)
-        installations += res.items
-    return installations
+    return await InstallationDocument.find_all().to_list()
 
 
-@app.get("/public/installations/count", tags=["installation", "open"])
-def get_installation_count():
+@public_router.get("/installations/count", tags=["installation"])
+async def get_installation_count():
     """Endpoint to receive information about successful installation count.
 
     Route is open accessible.
     """
-    res = installation_deta.fetch(limit=10000)
-    installations: list = res.items
-    while res.last:
-        res = installation_deta.fetch(limit=10000, last=res.last)
-        installations += res.items
-    return len(installations)
+    installation_data = await InstallationDocument.find_all().to_list()
+    return len(installation_data)
 
 
-@app.post("/__space/v0/actions", tags=["automation", "protected"])
-def run_actions(event_object: DetaEvent) -> None:
-    """Route which is triggered on deta action.
-
-    The event data with id / trigger is provided in the event object.
-    """
-    event = event_object.event
-    if event.id == "cleanup":
-        to_delete: list[dict] = cocktail_deta.fetch({"cocktailname?contains": "testcocktail"}).items
-        if len(to_delete) > 0:
-            logger.warning("Deleting %s number of items named testcocktail", len(to_delete))
-        for cocktail in to_delete:
-            logger.warning("Deleting item: %s", cocktail)
-            cocktail_deta.delete(key=cocktail["key"])
+# TODO: Implement a scheduled cleanup task
+async def run_cleanup() -> None:
+    """Route which is triggered on deta action."""
+    to_delete: list[CocktailDocument] = []
+    if len(to_delete) > 0:
+        logger.warning("Deleting %s number of items named testcocktail", len(to_delete))
+    for cocktail in to_delete:
+        logger.warning("Deleting item: %s", cocktail)
+        await cocktail.delete()
