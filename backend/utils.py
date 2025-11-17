a2084740860911de3b9c2c2588d828db184fc63e
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2025-11-17 20:05:32 +0100

    use default logging library
    fastapi logger does not use set up logging config

--- backend/utils.py
+++ backend/utils.py
@@ -1,19 +1,21 @@
+import logging
 import re
 
-from fastapi.logger import logger
 from fastapi_utilities import repeat_every
 from models import CocktailDocument
+
+_logger = logging.getLogger(__name__)
 
 
 @repeat_every(seconds=60 * 20)  # 20 minutes
 async def run_cleanup() -> None:
     """Route which is triggered on deta action."""
-    logger.warning("Running cleanup")
+    _logger.warning("Running cleanup")
     to_delete: list[CocktailDocument] = await CocktailDocument.find(
         {"cocktailname": re.compile("testcocktail", re.IGNORECASE)}
     ).to_list()
     if len(to_delete) > 0:
-        logger.warning("Deleting %s number of items named testcocktail", len(to_delete))
+        _logger.warning("Deleting %s number of items named testcocktail", len(to_delete))
     for cocktail in to_delete:
-        logger.warning("Deleting item: %s", cocktail)
+        _logger.warning("Deleting item: %s", cocktail)
         await cocktail.delete()
